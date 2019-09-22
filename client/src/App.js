d11e49afce5d30bc7fc3cd4b1562372d93d4ce06
blaze <kontaktAK@Yandex.ru>
2019-09-22 21:51:30 +0300

    form

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import FileUpload from './components/FileUpload'
 
 import './App.css'
 
-const App = () => <div className="App"> app </div>
+const App = () => <div className="container mt-4">
+
+  <h4 className="display-4 text-center mb-4">
+    <i className="fab fa-react"/> React file upload
+  </h4>
+  <FileUpload/>
+</div>
 
 export default App
---
+++ client/src/components/FileUpload.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react'
+
+function FileUpload (props) {
+
+  return (
+    <Fragment>
+
+      <form>
+        <div className="custom-file">
+          <input type="file" className="custom-file-input" id="customFile"/>
+          <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+        </div>
+        <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4"/>
+      </form>
+
+
+    </Fragment>
+  )
+}
+
+export default FileUpload
